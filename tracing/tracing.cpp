454bc45c847ee6050cb213f1041e96911db55743
Mihail Shukin <mihailshukin@yandex.ru>
2022-01-31 16:24:16 +0300

    tracing

---
+++ tracing/tracing.cpp
@@ -0,0 +1,60 @@
+﻿// tracing.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/video.hpp>
+using namespace cv;
+using namespace std;
+int main()
+{
+	VideoCapture capture(samples::findFile("train.mov"));
+	if (!capture.isOpened()) {
+		cerr << "Unable to open file!" << endl;
+		return 0;
+	}
+	Mat frame1, prvs;
+	capture >> frame1;
+	cvtColor(frame1, prvs, COLOR_BGR2GRAY);
+	while (true) {
+		Mat frame2, next;
+		capture >> frame2;
+		if (frame2.empty())
+			break;
+		cvtColor(frame2, next, COLOR_BGR2GRAY);
+		Mat flow(prvs.size(), CV_32FC2);
+		calcOpticalFlowFarneback(prvs, next, flow, 0.8, 4, 16, 4, 7, 1.3, 0);
+		Mat flow_parts[2];
+		split(flow, flow_parts);
+		Mat magnitude, angle, magn_norm;
+		cartToPolar(flow_parts[0], flow_parts[1], magnitude, angle, true);
+		normalize(magnitude, magn_norm, 0.0f, 1.0f, NORM_MINMAX);
+		angle *= ((1.f / 360.f) * (180.f / 255.f));
+		Mat _hsv[3], hsv, hsv8, bgr;
+		_hsv[0] = angle;
+		_hsv[1] = Mat::ones(angle.size(), CV_32F);
+		_hsv[2] = magn_norm;
+		merge(_hsv, 3, hsv);
+		hsv.convertTo(hsv8, CV_8U, 255.0);
+		cvtColor(hsv8, bgr, COLOR_HSV2BGR);
+		imshow("frame2", bgr);
+		int keyboard = waitKey(30);
+		if (keyboard == 'q' || keyboard == 27)
+			break;
+		prvs = next;
+	}
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
